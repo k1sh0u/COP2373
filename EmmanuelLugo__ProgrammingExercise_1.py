315d7ba964afec8b6440592007da835d1988641b
manny lugo <emmanuellugo84@gmail.com>
2026-06-03 21:23:25 -0400

    added function to compare tickets ordered to the number to tickets available.

--- EmmanuelLugo__ProgrammingExercise_1.py
+++ EmmanuelLugo__ProgrammingExercise_1.py
@@ -19,20 +19,26 @@ Submit both your .py file and .doc/.docx file in this assignment and these files
 
 ticket_supply = 20
 
-def ticket_tracker():
-    if ticket_supply < 0:
-        return "Sold out!"
+def ticket_tracker(tickets_ordered):
+    if tickets_ordered >= ticket_supply:
+        return True
+    else:
+        return False
+
+
 
 def buy_a_ticket():
+    print(f"Hello, there are currently {ticket_supply} tickets available.")
+
     tickets_to_buy = int(input:("How many tickets would you like?"))
+    if not ticket_tracker(tickets_to_buy):
+        return "sold out."
+    elif tickets_to_buy > 4:
+        return "4 tickets Max per customer."
+    else:
+        return f"You have purchased {tickets_to_buy} tickets."
+        ticket_supply -= tickets_to_buy
 
-    if tickets_to_buy > 4:
-        return "You can only purchase 4 tickets at one time."
-
-    ticket_supply -= tickets_to_buy
-
-
-ticket_tracker()
 
 buy_a_ticket()
 
