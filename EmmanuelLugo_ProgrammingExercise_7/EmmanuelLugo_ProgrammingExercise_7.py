92fcbf2f22e444762d3e2e177c8ff2e135419f1d
manny lugo <emmanuellugo84@gmail.com>
2026-06-27 13:04:35 -0400

    updated to include pseudo code and framework.

---
+++ EmmanuelLugo_ProgrammingExercise_7/EmmanuelLugo_ProgrammingExercise_7.py
@@ -0,0 +1,9 @@
+'''
+Using the code in Section 7.4, write a program that will allow me to enter a paragraph, including sentences which begin with numbers. Display each individual sentence and the count of sentences in the paragraph.
+
+You should have at least two functions, but you could have more.
+
+
+
+
+'''
