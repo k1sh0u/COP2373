3a863df5173000b3e37d901a142ecea950d6dcb6
manny lugo <emmanuellugo84@gmail.com>
2026-06-28 23:22:36 -0400

    Pretty easy. Implemented same code in textbook. Final draft will have to do.

--- EmmanuelLugo_ProgrammingExercise_7/EmmanuelLugo_ProgrammingExercise_7.py
+++ EmmanuelLugo_ProgrammingExercise_7/EmmanuelLugo_ProgrammingExercise_7.py
@@ -22,7 +22,32 @@ def capture_sentence():
 def seperate_sentence():
     this function will seperate each sentence, maybe at a period.?"
 
-    
+
 
 
 '''
+
+import re
+
+def main():
+
+    paragraph = input("Please enter a paragraph: ")
+
+    sentences = seperate_sentences(paragraph)
+    display_sentence(sentences)
+
+
+def seperate_sentences(paragraph):
+    pattern = r'[A-Z0-9].*?[.!?](?= [A-Z0-9]|$)'
+    sentences = re.findall(pattern, paragraph, flags=re.DOTALL | re.MULTILINE)
+    return sentences
+
+def display_sentence(sentence_list):
+    for i in sentence_list:
+        print(i)
+
+    print(f"the total number of sentences is {len(sentence_list)}")
+
+
+if __name__ == '__main__':
+    main()
