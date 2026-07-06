e1cf7c8d57bfb0fe1b01e419a84cdc6e4d3db73d
manny lugo <emmanuellugo84@gmail.com>
2026-07-05 23:24:29 -0400

    fixed single cell issue

--- EmmanuelLugo_ProgrammingExercise_CSV/EmmanuelLugo_ProgrammingExercise_CSV.py
+++ EmmanuelLugo_ProgrammingExercise_CSV/EmmanuelLugo_ProgrammingExercise_CSV.py
@@ -13,6 +13,8 @@ Submit both your .py file and .doc/.docx file in this assignment and these files
 ***The .csv file for this assignment should also be in your repository.
 '''
 
+
+
 %%writefile program1.py
 
 import csv
@@ -20,29 +22,28 @@ def write_to_csv_file():
     with open('grades.csv', 'w') as file:
         write_data = file.write("First Name, Last Name, Exam 1, Exam 2, Exam3\n")
 
-    while True:
-        try:
-            student_count = int(input("Enter count of students: "))
-            if student_count <= 0:
-                print("Please enter a positive count of students")
-                continue
-            break
+        while True:
+            try:
+                student_count = int(input("Enter count of students: "))
+                if student_count <= 0:
+                    print("Please enter a positive count of students")
+                    continue
+                break
 
-        except ValueError:
-            print("Please enter a valid count of student.")
+            except ValueError:
+                print("Please enter a valid count of student.")
 
-    for i in range(student_count):
-        first_name = input("Enter first name: ")
-        last_name = input("Enter last name: ")
-        exam_1 = int(input("Enter exam 1: "))
-        exam_2 = int(input("Enter exam 2: "))
-        exam_3 = (input("Enter exam 3: "))
+        for i in range(student_count):
+            first_name = input("Enter first name: ")
+            last_name = input("Enter last name: ")
+            exam_1 = int(input("Enter exam 1: "))
+            exam_2 = int(input("Enter exam 2: "))
+            exam_3 = (input("Enter exam 3: "))
 
-        row = f"{first_name} {last_name} {exam_1} {exam_2} {exam_3}"
+            row = f"{first_name},{last_name},{exam_1},{exam_2},{exam_3}"
+            file.write(row)
 
 write_to_csv_file()
-
-
 
 
 
--- EmmanuelLugo_ProgrammingExercise_CSV/program1.py
+++ EmmanuelLugo_ProgrammingExercise_CSV/program1.py
@@ -1,28 +1,29 @@
 
+
 import csv
 def write_to_csv_file():
     with open('grades.csv', 'w') as file:
         write_data = file.write("First Name, Last Name, Exam 1, Exam 2, Exam3\n")
 
-    while True:
-        try:
-            student_count = int(input("Enter count of students: "))
-            if student_count <= 0:
-                print("Please enter a positive count of students")
-                continue
-            break
+        while True:
+            try:
+                student_count = int(input("Enter count of students: "))
+                if student_count <= 0:
+                    print("Please enter a positive count of students")
+                    continue
+                break
 
-        except ValueError:
-            print("Please enter a valid count of student.")
+            except ValueError:
+                print("Please enter a valid count of student.")
 
-    for i in range(student_count):
-        first_name = input("Enter first name: ")
-        last_name = input("Enter last name: ")
-        exam_1 = int(input("Enter exam 1: "))
-        exam_2 = int(input("Enter exam 2: "))
-        exam_3 = (input("Enter exam 3: "))
+        for i in range(student_count):
+            first_name = input("Enter first name: ")
+            last_name = input("Enter last name: ")
+            exam_1 = int(input("Enter exam 1: "))
+            exam_2 = int(input("Enter exam 2: "))
+            exam_3 = (input("Enter exam 3: "))
 
-        row = f"{first_name} {last_name} {exam_1} {exam_2} {exam_3}"
+            row = f"{first_name} {last_name} {exam_1} {exam_2} {exam_3}"
+            file.write(row)
 
 write_to_csv_file()
-
