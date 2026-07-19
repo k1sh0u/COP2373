ac7c6a7d083aaa7bea551906b24307829e4204eb
manny lugo <emmanuellugo84@gmail.com>
2026-07-19 16:22:25 -0400

    updated comments

---
+++ EmmanuelLugo_ProgrammingExercise_11/EmmanuelLugo_ProgrammingExercise_11.py
@@ -0,0 +1,8 @@
+'''
+Using the Deck object presented in Section 11.5, write a game program that deals a Poker hand of five cards. Then prompt the user to enter a series of numbers (for example: 1, 3, 5) that selects cards to be replaced during a draw phrase. Then print the result of drawing the new cards.
+
+You should have at least two functions, but you can have more.
+
+Submit your .py file in this assignment and in your repository. 
+'''
+
