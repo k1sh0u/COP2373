488b3a315cdc9990997bcf398d2de1ced5eaf2f0
manny lugo <emmanuellugo84@gmail.com>
2026-07-19 20:46:34 -0400

    initialized the deck of card class

--- EmmanuelLugo_ProgrammingExercise_11/EmmanuelLugo_ProgrammingExercise_11.py
+++ EmmanuelLugo_ProgrammingExercise_11/EmmanuelLugo_ProgrammingExercise_11.py
@@ -3,6 +3,13 @@ Using the Deck object presented in Section 11.5, write a game program that deals
 
 You should have at least two functions, but you can have more.
 
-Submit your .py file in this assignment and in your repository. 
+Submit your .py file in this assignment and in your repository.
 '''
 
+import random
+
+class Deck():
+    def __init__(self, n_decks = 1):
+        self.n_decks = n_decks
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        rank =[2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
