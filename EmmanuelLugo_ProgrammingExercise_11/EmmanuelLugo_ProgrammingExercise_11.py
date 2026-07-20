cd01ae13c234fd64c6c15dd3059e5303897391a6
manny lugo <emmanuellugo84@gmail.com>
2026-07-19 22:00:32 -0400

    created the class, still working on the functions, commit before break

--- EmmanuelLugo_ProgrammingExercise_11/EmmanuelLugo_ProgrammingExercise_11.py
+++ EmmanuelLugo_ProgrammingExercise_11/EmmanuelLugo_ProgrammingExercise_11.py
@@ -9,7 +9,30 @@ Submit your .py file in this assignment and in your repository.
 import random
 
 class Deck():
-    def __init__(self, n_decks = 1):
-        self.n_decks = n_decks
-        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-        rank =[2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+    def __init__(self, size):
+        self.cards = [i for i in range(size)]
+        random.shuffle(self.cards)
+        self.cards_in_play =[]
+        self.cards_discarded = []
+
+    def deal(self):
+        if(len(self.cards)) < 1:
+            random.shuffle(self.cards_discarded)
+            self.cards = self.cards_discarded
+            self.cards_discarded = []
+        new_card = self.cards.pop()
+        self.cards_in_play.append(new_card)
+        return new_card
+
+    def new_hand(self):
+        self.cards_discards += self.cards_in_play
+        self.cards_in_play.clear()
+
+def poker(deck):
+    hand = []
+    for card in range(5):
+        hand.append(deck.deal())
+    print(hand)
+
+    user_discarded = input("Discard which cards? Choose 3 and enter the position of the cards, not the actual cards)")
+    
