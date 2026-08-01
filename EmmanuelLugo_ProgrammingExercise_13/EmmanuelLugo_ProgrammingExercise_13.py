fd474d8b3bc55230fd66411ebda131c823a917c6
manny lugo <emmanuellugo84@gmail.com>
2026-08-01 15:35:24 -0400

    created the directory and .py file for EmmanuelLugo_ProgrammingExercise_13

---
+++ EmmanuelLugo_ProgrammingExercise_13/EmmanuelLugo_ProgrammingExercise_13.py
@@ -0,0 +1,12 @@
+'''
+Write a program (function) to create a database called population_(your initials). For ex: population_SM would be my database. Create a table named population with the following fields; 1. city, 2. year, 3. population. Choose 10 cities in Florida and insert data into the population table for the year 2023.
+
+Create a function to the simulate population growth and decline for the next 20 years at various rates for each year. Insert this data into the population table.
+
+Using matplotlib, create a function to show the population growth for a city. Let the user know the 10 cities as options and ask them to choose one and display the population growth for the city visually.
+
+This assignment will have at least three functions.
+
+Submit your .py file in this assignment and in your repository.
+'''
+
